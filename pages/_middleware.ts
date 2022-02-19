7450be05f842a542b320f9fbebffd67d2589856a
fukata <tatsuya.fukata@gmail.com>
2022-02-19 23:52:43 +0900

    fix: next 12.1.0に対応

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import {NextURL} from "next/dist/server/web/next-url";
 
 export default function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /login)
@@ -8,9 +9,9 @@ export default function middleware(req: NextRequest) {
     // ログイン画面かつ認証済み
     if (req.cookies["next-auth.session-token"] || req.cookies["__Secure-next-auth.session-token"]) {
       if (pathname === '/' || pathname === '/login') {
-        return NextResponse.redirect("/dashboard");
+        return NextResponse.redirect(new NextURL("/dashboard", req.url));
       }
     }
-    return NextResponse.rewrite(`/app${pathname}`);
+    return NextResponse.rewrite(new NextURL(`/app${pathname}`, req.url));
   }
 }
